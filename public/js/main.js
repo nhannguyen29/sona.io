9fc46e678c98c0f4ee5a74a1fead1bc215f08f63
Anh Nguyen <dnna2610@gmail.com>
2016-08-25 10:08:26 -0400

    Like count on most favourtie page

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,17 @@
-function like(song_id, user_id) {
+function like(song_id, user_id, like_count) {
+    var url_like = "/songs/like/" + song_id + "/" + user_id;
+    $.ajax({
+        url: url_like,
+        type: "POST",
+        success: function(result) {}
+    });
+    var btn = "#btn_" + song_id;
+    $(btn).removeClass("btn-primary");
+    $(btn).addClass("medium green");
+    $(btn).html("You and " + like_count + " others liked this.");
+};
+
+function like_no_count(song_id, user_id) {
     var url_like = "/songs/like/" + song_id + "/" + user_id;
     $.ajax({
         url: url_like,
